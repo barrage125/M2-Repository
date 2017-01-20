756dfa1f71a7d5f85c3a05fac81e8ef4f488575f
Lotz, Alexander Michael <alexlotz@gatech.edu>
2017-01-20 12:38:42 -0500

    Person1.java method finished

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,10 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String last = input.substring(0,2);
+	  String first = input.substring(2);
+	  String output = first + last;
+	  return output;
 	}
 	
 	/**
